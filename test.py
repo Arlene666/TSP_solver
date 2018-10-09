563222eba4a3fdcc9a69f99c7412d164cf3cef83
Yifan Zhang <yifan.zhang@uwaterloo.ca>
2018-10-09 16:35:14 -0400

    changed test

--- test.py
+++ test.py
@@ -1,21 +1,21 @@
 import numpy as np
 import os
-#import tsp_noheu
+import tsp
 import fnmatch
 import matplotlib.pyplot as plt
 
 import math
 
-avg_generate = np.zeros((8, 1))
+avg_generate = np.zeros((10, 1))
 
 nomatch = ['.DS_Store', 'problem36', '16', '15', '14', '13', '12', '11', '10', '9']
 
 '''for folder in os.listdir('tsp_problems'):
     if not str(folder) in nomatch:
         print(folder)
+'''
 
-
-
+count = 0
 for myfile in os.listdir('tsp_problems/8'):
     f = open(os.path.join('tsp_problems/8', myfile))
     content = f.readlines()
@@ -30,11 +30,15 @@ for myfile in os.listdir('tsp_problems/8'):
         y = int(t[2])
         points.append((city, x, y))
 
-    path, weight, generate = tsp_noheu.tsp_noheu(points)
+    path, weight, generate = tsp.tsp(points)
     f.close()
-    print(generate)
-'''
-x = math.log(10/10)
-print(x)
+    avg_generate[count] = generate
+    count += 1
 
-math.
+    print(generate)
+
+plt.plot(range(10), avg_generate)
+
+
+
+
